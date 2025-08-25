bf9ceb572ac4f3b6caf6a4c825f82f0e5e1fae62
Joel Hägvall <joel.hagvall1@gmail.com>
2025-08-25 11:01:28 +0200

    Enhance sitemap configuration with custom change frequency and priority settings for specific paths

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -3,6 +3,33 @@ module.exports = {
   siteUrl: 'https://joelhagvall.com',
   generateRobotsTxt: true,
   sitemapSize: 7000,
+  transform: async (config, path) => {
+    // Defaults
+    let changefreq = 'daily';
+    let priority = 0.7;
+
+    if (path === '/') {
+      changefreq = 'daily';
+      priority = 1.0;
+    } else if (path === '/projects') {
+      changefreq = 'weekly';
+      priority = 0.6;
+    } else if (path === '/resume') {
+      changefreq = 'monthly';
+      priority = 0.5;
+    } else if (path === '/about') {
+      changefreq = 'monthly';
+      priority = 0.5;
+    }
+
+    return {
+      loc: path,
+      changefreq,
+      priority,
+      lastmod: new Date().toISOString(),
+      alternateRefs: config.alternateRefs ?? [],
+    };
+  },
 };
 
 
