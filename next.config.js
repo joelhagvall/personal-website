0d8da2fef2213b5356d741cd874642bea4aa1f95
Joel Hägvall <joel.hagvall1@gmail.com>
2025-09-14 20:08:09 +0200

    Update Next.js configuration to enable unoptimized images for improved development experience.

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
-  images: { unoptimized: false },
+  images: { unoptimized: true },
   async headers() {
     return [
       {
